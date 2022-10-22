3731c8bfb81a235ce3e2775c2ec0b79773eb4488
z <alex@dwfe.ru>
2022-10-22 15:41:13 +0300

    .

--- src/noop.ts
+++ src/noop.ts
@@ -1,27 +1,20 @@
-//@ts-ignore
-export function noop<T = any, R = T>(data?: T): R {
+export function noop(...data: any[]): any {
 }
 
-//@ts-ignore
-export function noop2<T = any, R = T>(data?: T): R {
+export function noop2(...data: any[]): any {
 }
 
-//@ts-ignore
-export function noop3<T = any, R = T>(data?: T): R {
+export function noop3(...data: any[]): any {
 }
 
-//@ts-ignore
-export function noop4<T = any, R = T>(data?: T): R {
+export function noop4(...data: any[]): any {
 }
 
-//@ts-ignore
-export function noop5<T = any, R = T>(data?: T): R {
+export function noop5(...data: any[]): any {
 }
 
-//@ts-ignore
-export function noop6<T = any, R = T>(data?: T): R {
+export function noop6(...data: any[]): any {
 }
 
-//@ts-ignore
-export function noop7<T = any, R = T>(data?: T): R {
+export function noop7(...data: any[]): any {
 }
