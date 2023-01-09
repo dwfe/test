f47ce4e18a7059f5952e301b5806c10310373279
z <alex@dwfe.ru>
2023-01-09 22:17:33 +0300

    .

--- src/noop.ts
+++ src/noop.ts
@@ -1,20 +1,27 @@
+// @ts-ignore
 export function noop(...data: any[]): any {
 }
 
+// @ts-ignore
 export function noop2(...data: any[]): any {
 }
 
+// @ts-ignore
 export function noop3(...data: any[]): any {
 }
 
+// @ts-ignore
 export function noop4(...data: any[]): any {
 }
 
+// @ts-ignore
 export function noop5(...data: any[]): any {
 }
 
+// @ts-ignore
 export function noop6(...data: any[]): any {
 }
 
+// @ts-ignore
 export function noop7(...data: any[]): any {
 }
